ea00f2e39a6a5494042f6a35d07e6c70062c5dc6
Chris Sung Won Lee <sungwlee8@gmail.com>
2020-09-05 00:40:26 +0000

    commits

---
+++ node-notes-cli/add.js
@@ -0,0 +1,11 @@
+// const fs = require('fs');
+
+// const objj = require('json');
+// const obj = { objj.nextID: process.argv[2] };
+// objStr = JSON.stringify(obj);
+
+// fs.writeFile('data.json', objStr, 'utf8', err => {
+//   if (err) {
+//     throw err;
+//   }
+// });
---
+++ node-notes-cli/read.js
@@ -0,0 +1,14 @@
+// const fs = require('fs');
+
+// let parsedData;
+
+// fs.readFile('data.json', 'utf8', (err, data) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     parsedData = JSON.parse(data);
+//     for (const property in parsedData.notes) {
+//       console.log(`${property}: ${parsedData.notes[property]}`);
+//     }
+//   }
+// });
