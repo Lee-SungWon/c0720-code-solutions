d69d7636f3e96000ca67fba3debd1e3ee32492b1
Chris Sung Won Lee <sungwlee8@gmail.com>
2020-09-07 23:04:22 +0000

    completed array manipulation

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,14 +1,23 @@
 /* eslint-disable no-unused-vars */
-// function chunk(array, size) {
-//   let arr = [];
-//   let newArr = [];
-//   if (array.length > size) {
-//     for (let i = 0; i < size; i++){
-//       newArr.push(array[i]);
-//     }
-//     }
-//   } else {
-//     newArr = array;
-//   }
-//   return newArr;
-// }
+
+/*
+chunk(['foo', 'bar', 'baz', 'qux'], 2);
+// -> [["foo", "bar"], ["baz", "qux"]]
+chunk([1, 2, 3, 4, 5], 1);
+// -> [[1], [2], [3], [4], [5]]
+chunk([false, true, false, true], 3);
+// -> [[false, true, false], [true]]
+*/
+
+const chunk = (array, size) => {
+  let arr = [];
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    arr.push(array[i]);
+    if (arr.length === size || i === array.length - 1) {
+      newArr.push(arr);
+      arr = [];
+    }
+  }
+  return newArr;
+};
