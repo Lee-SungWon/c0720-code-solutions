356a3fd04a35901dcab0479397d67a011b02220b
Chris SW Lee <sungwlee8@gmail.com>
2020-09-08 22:22:39 -0700

    revised defaults

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -4,29 +4,38 @@
 Assigns properties of source object to the target object for all properties that are not defined on target.
 */
 
+// const defaults = (target, source) => {
+//   const targetProp = [];
+//   const sourceProp = [];
+
+//   for (const property in target) {
+//     targetProp.push(property);
+//   }
+
+//   for (const property in source) {
+//     sourceProp.push(property);
+//   }
+
+//   let counter = 0;
+//   for (let i = 0; i < sourceProp.length; i++) {
+//     for (let j = 0; j < targetProp.length; j++) {
+//       if (sourceProp[i] !== targetProp[j]) {
+//         counter++;
+//       }
+//     }
+//     if (counter === targetProp.length) {
+//       target[sourceProp[i]] = source[sourceProp[i]];
+//     }
+//     counter = 0;
+//   }
+//   return target;
+// };
+
 const defaults = (target, source) => {
-  const targetProp = [];
-  const sourceProp = [];
-
-  for (const property in target) {
-    targetProp.push(property);
-  }
-
   for (const property in source) {
-    sourceProp.push(property);
-  }
-
-  let counter = 0;
-  for (let i = 0; i < sourceProp.length; i++) {
-    for (let j = 0; j < targetProp.length; j++) {
-      if (sourceProp[i] !== targetProp[j]) {
-        counter++;
-      }
+    if (target[property] === undefined) {
+      target[property] = source[property];
     }
-    if (counter === targetProp.length) {
-      target[sourceProp[i]] = source[sourceProp[i]];
-    }
-    counter = 0;
   }
   return target;
 };
