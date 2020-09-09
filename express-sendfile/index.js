5d4d9b6dfe4963e221aad1e8142d6bf40e4e7ede
Chris Sung Won Lee <sungwlee8@gmail.com>
2020-09-09 18:13:12 +0000

    express sendfile

--- express-sendfile/index.js
+++ express-sendfile/index.js
@@ -3,9 +3,16 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  res.sendFile(req.originalUrl, { root: __dirname }, err => {
-    if (err) next();
-  });
+  const options = { root: __dirname };
+  if (req.originalURL === '/index.html') {
+    res.sendFile('/index.html', options);
+  } else if (req.originalUrl === '/styles.css') {
+    res.sendFile('/styles.css', options);
+  } else if (req.originalUrl === '/main.js') {
+    res.sendFile('/main.js', options);
+  } else {
+    next();
+  }
 });
 
 app.listen(3000, () => {
