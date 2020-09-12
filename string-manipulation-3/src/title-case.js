43d4219fc64b5ccd7cae3a5638de3d88d89a35ca
Chris Sung Won Lee <sungwlee8@gmail.com>
2020-09-12 17:09:12 +0000

    string manipulation 3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,14 +2,15 @@
 function titleCase(title) {
   const minor = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
   const arr = title.split(' ');
-  let newArr = [];
+  const newArr = [];
 
   for (let i = 0; i < arr.length; i++) {
     let newWord = '';
-    if (minor.includes(arr[i])) {
-      newWord += arr[i][0];
-    } else {
+
+    if (!minor.includes(arr[i]) || arr[i - 1].includes(':')) {
       newWord += arr[i][0].toUpperCase();
+    } else {
+      newWord += arr[i][0];
     }
     for (let j = 1; j < arr[i].length; j++) {
       newWord += arr[i][j];
@@ -18,11 +19,44 @@ function titleCase(title) {
   }
 
   for (let j = 0; j < newArr.length; j++) {
-    if (newArr[j].toLowerCase === 'javascript') {
-      const splice1 = newArr.splice(0, j);
-      splice1.push('JavaScript');
-      const splice2 = newArr.splice(1, newArr.length - 1);
-      newArr = splice1.concat(splice2);
+    if (newArr[j].includes('-')) {
+      const index = newArr[j].indexOf('-');
+      const wordArr = newArr[j].split('');
+      wordArr[index + 1] = wordArr[index + 1].toUpperCase();
+      const word = wordArr.join('');
+      newArr[j] = word;
+    }
+  }
+
+  for (let j = 0; j < newArr.length; j++) {
+    if (newArr[j].toLowerCase().includes('javascript')) {
+      let word;
+      if (newArr[j].includes(':')) {
+        word = 'JavaScript:';
+      } else {
+        word = 'JavaScript';
+      }
+      newArr[j] = word;
+      // const splice1 = newArr.splice(0, j);
+      // splice1.push(word);
+      // const splice2 = newArr.splice(1, newArr.length - 1);
+      // newArr = splice1.concat(splice2);
+    }
+  }
+
+  for (let j = 0; j < newArr.length; j++) {
+    if (newArr[j].toLowerCase().includes('api')) {
+      let word;
+      if (newArr[j].includes(':')) {
+        word = 'API:';
+      } else {
+        word = 'API';
+      }
+      newArr[j] = word;
+      // const splice1 = newArr.splice(0, j);
+      // splice1.push(word);
+      // const splice2 = newArr.splice(1, newArr.length - 1);
+      // newArr = splice1.concat(splice2);
     }
   }
 
