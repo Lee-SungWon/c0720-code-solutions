ac14499018fbb39ccd7b798621501efeac42235f
Chris Sung Won Lee <sungwlee8@gmail.com>
2020-09-15 22:29:48 +0000

    react rendering lists

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+function ListPokemons(props) {
+  const pokemons = props.pokedex;
+  const listPokemons = pokedex.map(pokemon =>
+    <li>{pokemon.name}</li>
+  );
+  return (
+    <ul>{listPokemons}</ul>
+  );
+}
+
+const container = document.getElementById('root');
+
+ReactDOM.render(
+  <ListPokemons />,
+  container
+);
