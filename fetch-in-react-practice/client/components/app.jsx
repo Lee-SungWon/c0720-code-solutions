df32e7ac29db3bdf6ee147c10df6575da634fa24
Chris Sung Won Lee <sungwlee8@gmail.com>
2020-09-21 23:48:39 +0000

    fetch in react practice

--- fetch-in-react-practice/client/components/app.jsx
+++ fetch-in-react-practice/client/components/app.jsx
@@ -18,39 +18,62 @@ class App extends React.Component {
   }
 
   getAllTodos() {
-    fetch('')
+    fetch('/api/todos')
       .then(res => res.json());
-    this.setState();
-    /**
-     * Use fetch to send a GET request to `/api/todos`.
-     * Then 😉, once the response JSON is received and parsed,
-     * update state with the received todos.
-     */
+      .then(res => {
+        this.setState({
+          todos: res
+        });
+      })
+      .catch(err => console.error('fetch failed!', err))
   }
 
   addTodo(newTodo) {
-    /**
-     * Use fetch to send a POST request to `/api/todos`.
-     * Then 😉, once the response JSON is received and parsed,
-     * add the created todo to the state array.
-     *
-     * TIP: Be sure to SERIALIZE the todo in the body with JSON.stringify()
-     * And specify the "Content-Type" header as "application/json"
-     */
+    fetch('/api/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type: 'application/json'
+      },
+      body: JSON.stringify(newTodo)
+      })
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          todos: this.state.todos.concat(res)
+        })
+      })
+      .catch(err => console.error('fetch failed!', err))
   }
 
+  /**
+   * Find the index of the matching todo in the state array.
+   * Find its "isCompleted" status.
+   * Make a new Object containing the opposite "isCompleted" status.
+   * Use fetch to send a PATCH request to `/api/todos/${todoId}`
+   * Then 😉, once the response JSON is received and parsed,
+   * replace the old todo in state.
+   *
+   * TIP: Be sure to SERIALIZE the updates in the body with JSON.stringify()
+   * And specify the "Content-Type" header as "application/json"
+   */
+
   toggleCompleted(todoId) {
-    /**
-     * Find the index of the matching todo in the state array.
-     * Find its "isCompleted" status.
-     * Make a new Object containing the opposite "isCompleted" status.
-     * Use fetch to send a PATCH request to `/api/todos/${todoId}`
-     * Then 😉, once the response JSON is received and parsed,
-     * replace the old todo in state.
-     *
-     * TIP: Be sure to SERIALIZE the updates in the body with JSON.stringify()
-     * And specify the "Content-Type" header as "application/json"
-     */
+    const arr = this.state.todos;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].id === todoId) {
+        if (!arr[i].isCompleted) {
+          arr[i].isCompleted = true;
+          fetch(`/api/todos/${todoId}`, {
+            method: "PATCH",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(arr[i])
+          })
+            .then(res => res.json())
+            .then(res => this.setState([{ todos: res }]))
+            .catch(err => console.error('fetch failed!', err))
+        }
+      }
+    }
   }
 
   render() {
@@ -58,9 +81,9 @@ class App extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col pt-5">
-            <PageTitle text="React Todo"/>
-            <TodoForm onSubmit={this.addTodo}/>
-            <TodoList todos={this.state.todos} toggleCompleted={this.toggleCompleted}/>
+            <PageTitle text="React Todo" />
+            <TodoForm onSubmit={this.addTodo} />
+            <TodoList todos={this.state.todos} toggleCompleted={this.toggleCompleted} />
           </div>
         </div>
       </div>
