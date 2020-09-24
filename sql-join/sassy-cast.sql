419912bc6ad3326c2291358e6b38fd544e9a1000
Chris Sung Won Lee <sungwlee8@gmail.com>
2020-09-24 19:08:15 +0000

    sql join

---
+++ sql-join/sassy-cast.sql
@@ -0,0 +1,7 @@
+select "firstName",
+        "lastName",
+        "films"."title"
+from "actors"
+join "castMembers" using ("actorId")
+join "films" using ("filmId")
+where "films"."title" = 'Jersey Sassy';
